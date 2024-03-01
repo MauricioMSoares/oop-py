aec8a47a7d4d73ee3d1aebe59412ed6bcc1e5d9b
MauricioMSoares <mauricio.soares@sptech.school>
2024-02-29 23:21:43 -0300

    feat: protected restaurants attributes and created person object

---
+++ models/person.py
@@ -0,0 +1,26 @@
+class Person:
+    def __init__(self, name, profession = 'None', age = 0):
+        self._name = name
+        self._profession = profession
+        self._age = age
+
+    def __str__(self):
+        return f'{self._name}, {self._profession}, {self._age} years old.'
+
+    @property
+    def salutation(self):
+        if self._profession != 'None':
+            return f"Hi, I'm {self._name}, I work as {self._profession}!"
+        else:
+            return f"Hi, I'm {self._name}"
+
+    def birthday(self):
+        self._age += 1
+
+person = Person('Naomi', 'Project Manager', 25)
+print(person)
+
+person.birthday()
+print(person)
+
+print(person.salutation)
--- models/restaurant.py
+++ models/restaurant.py
@@ -2,17 +2,27 @@ class Restaurant:
     restaurants = []
 
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
-        self.active = False
+        self._name = name.title()
+        self._category = category.upper()
+        self._active = False
         Restaurant.restaurants.append(self)
 
     def __str__(self):
         return f'{self.name} | {self.category} | {self.active}'
     
-    def list_restaurants():
-        for restaurant in Restaurant.restaurants:
-            print(f'{restaurant.name} | {restaurant.category} | {restaurant.active}')
+    @classmethod
+    def list_restaurants(cls):
+        print(f'{'Restaurant Name'.ljust(25)} | {'Category'.ljust(25)} | Status'.ljust(25))
+        for restaurant in cls.restaurants:
+            print(f'{restaurant._name.ljust(25)} | {restaurant._category.ljust(25)} | {restaurant.active.ljust(25)}')
 
-restaurant = Restaurant('Restaurant', 'Italian')
+    @property
+    def active(self):
+        return 'Active' if self._active else 'Inactive'
+    
+    def alter_state(self):
+        self._active = not self._active
+
+restaurant = Restaurant('Washoku no Ie', 'Japanese')
+restaurant.alter_state()
 Restaurant.list_restaurants()
